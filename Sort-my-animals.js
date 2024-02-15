783d8176d94dce999da9e7efd849bc8ba75c5667
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-02-15 13:55:36 -0800

    Create Sort-my-animals.js

---
+++ Sort-my-animals.js
@@ -0,0 +1,13 @@
+/*
+Consider the following class:
+  let Animal = { 
+      name: "Cat", 
+      numberOfLegs: 4 
+  }
+
+Write a method that accepts a list of objects of type Animal, and returns a new list. 
+The new list should be a copy of the original list, 
+sorted first by the animal's number of legs, and then by its name.
+
+If an empty list is passed in, it should return an empty list back.
+*/
